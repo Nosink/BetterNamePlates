f791cb8909de0cb1d158cc980e9f6db2ef8a4ee6
NosinkD <ricardruizc@gmail.com>
2026-02-08 10:44:56 +0100

    cxontrol castbar tick

--- controller/castBar.lua
+++ controller/castBar.lua
@@ -15,18 +15,43 @@ local function getCastInfo(unitToken)
     return string.format("%.1f/%.1f", elapsed / 1000, duration / 1000)
 end
 
-local function onCastBarShown(unitToken)
-    C_Timer.NewTicker(0.1, function() 
+local function clearTicker(namePlate)
+    if namePlate.castBarTicker then
+        namePlate.castBarTicker:Cancel()
+        namePlate.castBarTicker = nil
+    end
+end
+
+local function onCastBarShow(unitToken)
+    print ("Cast bar shown for " .. unitToken)
+    local namePlate = ns:GetNamePlate(unitToken)
+    if not namePlate then return end
+
+    clearTicker(namePlate)
+
+    local ticker = C_Timer.NewTicker(0.1, function() 
         local text = getCastInfo(unitToken)
         BNPBus:TriggerEvent(name .. "_CAST_LABEL_UPDATE_REQUEST", unitToken, text)
     end, nil)
+
+    namePlate.castBarTicker = ticker
+end
+
+local function onCastBarHide(unitToken)
+    print ("Cast bar hidden for " .. unitToken)
+    local namePlate = ns:GetNamePlate(unitToken)
+    if not namePlate then return end
+
+    clearTicker(namePlate)
 end
 
 local function onCastLabelReady(_, unitToken)
     local namePlate = ns:GetNamePlate(unitToken)
+    if not namePlate then return end
+
     local castLabel = namePlate.UnitFrame.castLabel
-    onCastBarShown(unitToken)
-    --BNPBus:HookScript(castLabel, "OnShow", function() onCastBarShown(unitToken) end)
+    BNPBus:HookScript(castLabel, "OnShow", function() onCastBarShow(unitToken) end)
+    BNPBus:HookScript(castLabel, "OnHide", function() onCastBarHide(unitToken) end)
 end
 
 BNPBus:RegisterEvent(name .. "_NAME_PLATE_CAST_LABEL_READY", onCastLabelReady)
